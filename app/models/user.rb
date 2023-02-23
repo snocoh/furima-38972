4c104fc2baa6370c8d0bbb6eb627aef1728779e2
snocoh <s.noco2.lvr@gmail.com>
2023-02-23 16:55:22 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
   validates :password, format: { with: PASSWORD_REGEX }
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :kana_family_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :birthday, presence: true
   
 

cadfc62afa338a29fd4d74669f82de914b63d445
snocoh <s.noco2.lvr@gmail.com>
2023-02-24 21:20:47 +0900

    テスト環境準備・userモデルテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.initials}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name {"山田"}
+    first_name {"陸太郎"}
+    kana_family_name {"ヤマダ"}
+    kana_first_name {"リタロウ"}
+    birthday {Faker::Date.birthday}    
+  end
+end

6826e3f78b232b177895281efe9e539d4aac52db
snocoh <s.noco2.lvr@gmail.com>
2023-02-26 19:40:40 +0900

    指摘修正

--- db/schema.rb
+++ db/schema.rb
@@ -36,13 +36,13 @@ ActiveRecord::Schema.define(version: 2023_02_25_065320) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "goods", null: false
     t.text "content", null: false
-    t.integer "category_id"
-    t.integer "condition_id"
-    t.integer "postage_id"
-    t.integer "prefecture_id"
-    t.integer "delivery_day_id"
-    t.integer "price"
-    t.bigint "user_id"
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivery_day_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -78,6 +78,19 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+
+      it 'priceは半角数字でなければ登録できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'userが紐づいていなければ登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
     end
   end
 end

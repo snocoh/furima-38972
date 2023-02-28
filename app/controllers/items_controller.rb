380033680b2d0efecf72fc1c37e99ef0f2737a9e
snocoh <s.noco2.lvr@gmail.com>
2023-02-28 21:35:28 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: :edit
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -40,13 +38,18 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :goods, :content, :category_id, :condition_id, :postage_id, :prefecture_id,
-                                 :delivery_day_id, :price).merge(user_id: current_user.id)
+                                 :delivery_day_id, :price).merge(user_id: current_user.id)                        
   end
 
   def move_to_index
     @item = Item.find(params[:id])
-    return if current_user.id == @item.user_id
-
-    redirect_to root_path
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end

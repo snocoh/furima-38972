df8ebf9edc3e0fa19e076afff4f62e5aecef3a8c
snocoh <s.noco2.lvr@gmail.com>
2023-03-01 19:04:22 +0900

    Formオブジェクトを用いた購入情報の登録機能

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order.id != nil
+      redirect_to root_path
+    else
+      @order_address = OrderAddress.new
+    end
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :street
+    validates :phone_number, numericality: {only_integer: true, length: {in: 10..11}, message: 'is invalid'}
+    validates :user_id
+    validates :item_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, street: street, building: building, phone_number: phone_number)
+  end
+end

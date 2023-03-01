5e1e48fbad84446ebb61c5b7180637b207dcbce6
snocoh <s.noco2.lvr@gmail.com>
2023-03-01 19:35:39 +0900

    再・購入ボタン表示分岐view修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id || @item.order.id != nil
+    if current_user.id == @item.user_id || @item.order != nil
       redirect_to root_path
     else
       @order_address = OrderAddress.new
